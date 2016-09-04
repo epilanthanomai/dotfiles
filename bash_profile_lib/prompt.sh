580860bb935b8c42261538aedd5aaf38a881971e
Hope Ranker <hope.ranker@gmail.com>
2016-09-04 17:10:15 -0400

    Add git branch to prompt

--- bash_profile_lib/prompt.sh
+++ bash_profile_lib/prompt.sh
@@ -1,5 +1,10 @@
 _PROMPT_GREEN="$(tput setaf 2)"
+_PROMPT_CYAN="$(tput setaf 6)"
 _PROMPT_RESET="$(tput op)"
+
+function _current_branch() {
+  git rev-parse --abbrev-ref HEAD 2>/dev/null
+}
 
 function _prompt_leader() {
   local virtualenv_leader=''
@@ -8,6 +13,12 @@ function _prompt_leader() {
     virtualenv_leader="$_PROMPT_GREEN$virtualenv_name$_PROMPT_RESET":
   fi
 
-  echo "$virtualenv_leader"
+  local branch_leader=''
+  local current_branch="$(_current_branch)"
+  if [ -n "$current_branch" ]; then
+    branch_leader="$_PROMPT_CYAN$current_branch$_PROMPT_RESET:"
+  fi
+
+  echo "$virtualenv_leader$branch_leader"
 }
 export PS1='$(_prompt_leader)\W\$ '
