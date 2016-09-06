e012717afaf1e2e6a6712c9d0918b8e970d627a7
Hope Ranker <hope.ranker@gmail.com>
2016-09-05 21:07:25 -0400

    Fix bash prompt nonprinting chars

--- bash_profile_lib/prompt.sh
+++ bash_profile_lib/prompt.sh
@@ -2,23 +2,7 @@ _PROMPT_GREEN="$(tput setaf 2)"
 _PROMPT_CYAN="$(tput setaf 6)"
 _PROMPT_RESET="$(tput op)"
 
-function _current_branch() {
-  git rev-parse --abbrev-ref HEAD 2>/dev/null
-}
+_prompt_branch='\['"$_PROMPT_GREEN"'\]${VIRTUAL_ENV##*/}\['"$_PROMPT_RESET"'\]:'
+_prompt_virtualenv='\['"$_PROMPT_CYAN"'\]$(git rev-parse --abbrev-ref HEAD 2>/dev/null)\['"$_PROMPT_RESET"'\]:'
 
-function _prompt_leader() {
-  local virtualenv_leader=''
-  if [ -n "$VIRTUAL_ENV" ]; then
-    local virtualenv_name="${VIRTUAL_ENV##*/}"
-    virtualenv_leader="$_PROMPT_GREEN$virtualenv_name$_PROMPT_RESET":
-  fi
-
-  local branch_leader=''
-  local current_branch="$(_current_branch)"
-  if [ -n "$current_branch" ]; then
-    branch_leader="$_PROMPT_CYAN$current_branch$_PROMPT_RESET:"
-  fi
-
-  echo "$virtualenv_leader$branch_leader"
-}
-export PS1='$(_prompt_leader)\W\$ '
+export PS1="$_prompt_branch$_prompt_virtualenv"'\W\$ '
