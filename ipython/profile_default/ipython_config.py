ea616472cdab7e089e87d6a3cad4af3fd2e4a418
Hope Ranker <hope.ranker@gmail.com>
2017-12-20 15:27:21 -0500

    Configure ipython

---
+++ ipython/profile_default/ipython_config.py
@@ -0,0 +1,8 @@
+c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.exec_lines=[
+    '%autoreload 2',
+]
+
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.highlighting_style = 'native'
