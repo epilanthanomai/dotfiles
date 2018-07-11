71cf39ddca2ec091fc8dbba1d55f0e72a0621498
Hope Ranker <hope.ranker@gmail.com>
2018-07-11 11:25:43 -0400

    Correct branch/virtualenv internal names in path

--- bash_profile_lib/prompt.sh
+++ bash_profile_lib/prompt.sh
@@ -2,7 +2,7 @@ _PROMPT_GREEN="$(tput setaf 2)"
 _PROMPT_CYAN="$(tput setaf 6)"
 _PROMPT_RESET="$(tput op)"
 
-_prompt_branch='\['"$_PROMPT_GREEN"'\]${VIRTUAL_ENV##*/}\['"$_PROMPT_RESET"'\]:'
-_prompt_virtualenv='\['"$_PROMPT_CYAN"'\]$(git rev-parse --abbrev-ref HEAD 2>/dev/null)\['"$_PROMPT_RESET"'\]:'
+_prompt_virtualenv='\['"$_PROMPT_GREEN"'\]${VIRTUAL_ENV##*/}\['"$_PROMPT_RESET"'\]:'
+_prompt_branch='\['"$_PROMPT_CYAN"'\]$(git rev-parse --abbrev-ref HEAD 2>/dev/null)\['"$_PROMPT_RESET"'\]:'
 
-export PS1="$_prompt_branch$_prompt_virtualenv"'\W\$ '
+export PS1="$_prompt_virtualenv$_prompt_branch"'\W\$ '
