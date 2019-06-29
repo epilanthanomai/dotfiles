3fcb6e9cec33a67600041b544ef67d92f6c39019
Hope Ranker <hope.ranker@gmail.com>
2019-06-29 13:12:54 -0400

    local ca bundle for python requests in Darwin

--- bash_profile_lib/Darwin.sh
+++ bash_profile_lib/Darwin.sh
@@ -1,3 +1,6 @@
 # Why this value? Pandora dev-setup says it's the largest the kernel will
 # allow us *shrug*
 ulimit -SHn 10240
+
+# Tell Python requests to use the system CA bundle to support local CAs
+[ -r /usr/local/etc/openssl/cert.pem ] && export REQUESTS_CA_BUNDLE='/usr/local/etc/openssl/cert.pem'
