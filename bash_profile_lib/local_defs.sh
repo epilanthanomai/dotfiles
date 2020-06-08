c0c62bddca8c52d05be06b6dd2dc2a9080c463ee
Hope Ranker <hope.ranker@gmail.com>
2020-06-08 13:49:19 -0400

    don't track local bash settings

--- bash_profile_lib/local_defs.sh
+++
@@ -1,7 +0,0 @@
-_dora_completion()
-{
-    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
-                   COMP_CWORD=$COMP_CWORD \
-                   DORA_AUTO_COMPLETE=1 $1 ) )
-}
-complete -o default -F _dora_completion dora
--- bash_profile_lib/local_env.sh
+++
@@ -1,2 +0,0 @@
-export P4PORT=ssl:perforce.savagebeast.com:1666
-export P4USER="$USER"
