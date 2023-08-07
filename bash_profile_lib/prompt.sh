de8cabe0577c9f2f48985d78f172bfd22b726309
Hope Ranker <hope.ranker@gmail.com>
2023-08-07 16:43:24 -0400

    show stashed change count in bash prompt

--- bash_profile_lib/prompt.sh
+++ bash_profile_lib/prompt.sh
@@ -1,8 +1,11 @@
 _PROMPT_GREEN="$(tput setaf 2)"
+_PROMPT_YELLOW="$(tput setaf 3)"
+_PROMPT_MAGENTA="$(tput setaf 5)"
 _PROMPT_CYAN="$(tput setaf 6)"
 _PROMPT_RESET="$(tput op)"
 
 _prompt_virtualenv='\['"$_PROMPT_GREEN"'\]${VIRTUAL_ENV##*/}\['"$_PROMPT_RESET"'\]:'
 _prompt_branch='\['"$_PROMPT_CYAN"'\]$(git rev-parse --abbrev-ref HEAD 2>/dev/null)\['"$_PROMPT_RESET"'\]:'
+_prompt_stash='\['"$_PROMPT_YELLOW"'\]$(git rev-list --walk-reflogs --count refs/stash 2>/dev/null)\['"$_PROMPT_RESET"'\]:'
 
-export PS1="$_prompt_virtualenv$_prompt_branch"'\W\$ '
+export PS1="$_prompt_virtualenv$_prompt_branch$_prompt_stash"'\W\$ '
